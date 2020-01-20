885389c989afeec47f5cecbb0979dcebbb3e43a5
sannaman1 <sandeeprao.annamaneni@ge.com>
2020-01-21 00:48:34 +0530

    microservices for docker and k8s

---
+++ ms4/src/test/java/com/ms/ms4/Ms4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.ms4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
