28b3dbccc8647279d57915b9a26d4e28b6c9a595
sannaman1 <sandeeprao.annamaneni@ge.com>
2020-01-25 21:38:23 +0530

    updated

--- ms1/src/main/java/com/ms/ms1/MS2FeignClient.java
+++ ms1/src/main/java/com/ms/ms1/MS2FeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms2", url = "http://localhost:9092")
+@FeignClient(name = "ms2", url = "http://ms2:9092")
 public interface MS2FeignClient {
 
 	@GetMapping("/ms2/guest/address/{id}")
